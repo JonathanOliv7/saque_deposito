fd45807d6e37d2f37c70de988ff6a7b953bc98bb
Jonathan Oliveira <jonathanoliv7@gmail.com>
2026-02-10 20:21:45 -0300

    inicio

--- src/program/App.java
+++ src/program/App.java
@@ -1,10 +1,34 @@
 package program;
 
+import java.util.Scanner;
+
+import model.entitties.Account;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		try {
+		System.out.println("Enter account data: ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		Double balance = sc.nextDouble();
+		System.out.print("Withdraw limit");
+		Double withdrawLimit = sc.nextDouble();
+		
+		Account account = new Account( number, holder, balance, withdrawLimit);
+		
+		}
+		catch(RuntimeException e) {
+			
+		}
+		
+		
+		
+		sc.close();
 	}
-
 }
